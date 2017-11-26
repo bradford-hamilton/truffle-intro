87e438e8425f173c3847566cedbc5933e6f505f7
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-11-25 20:23:18 -0700

    comment

--- contracts/Greetings.sol
+++ contracts/Greetings.sol
@@ -11,6 +11,8 @@ contract Greetings {
     message = _message;
   }
 
+  // constants will not cost any gas and cannot change state of contract
+  // more like a read from a local db
   function getGreetings() public constant returns (string) {
     return message;
   }
